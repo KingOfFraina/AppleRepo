b33eb3f2bb97dc61dfa1f1e8c9f54f198c6e17fc
KingOfFraina <francesco.expo00@gmail.com>
2022-07-14 10:33:57 +0200

    Initial Commit

---
+++ AppFinale/AppFinaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFinaleApp.swift
+//  AppFinale
+//
+//  Created by Francesco Esposito on 14/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AppFinaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
